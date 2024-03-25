bdec3734036d22601cbc105ecbf55ee62d2eb386
RuslanAgishev <agishrus@fel.cvut.cz>
2024-03-25 11:41:55 +0100

    node to project point cloud to images

---
+++ src/dataproc/imgproc.py
@@ -0,0 +1,106 @@
+"""
+Copyright (C) 2020 NVIDIA Corporation.  All rights reserved.
+Licensed under the NVIDIA Source Code License. See LICENSE at https://github.com/nv-tlabs/lift-splat-shoot.
+Authors: Jonah Philion and Sanja Fidler
+"""
+
+import numpy as np
+import torch
+import torchvision
+from PIL import Image
+
+
+def ego_to_cam(points, rot, trans, intrins):
+    """Transform points (3 x N) from ego frame into a pinhole camera
+    """
+    points = points - trans.unsqueeze(1)
+    points = rot.permute(1, 0).matmul(points)
+
+    points = intrins.matmul(points)
+    points[:2] /= points[2:3]
+
+    return points
+
+
+def cam_to_ego(points, rot, trans, intrins):
+    """Transform points (3 x N) from pinhole camera with depth
+    to the ego frame
+    """
+    points = torch.cat((points[:2] * points[2:3], points[2:3]))
+    points = intrins.inverse().matmul(points)
+
+    points = rot.matmul(points)
+    points += trans.unsqueeze(1)
+
+    return points
+
+
+def get_only_in_img_mask(pts, H, W):
+    """pts should be 3 x N
+    """
+    return (pts[2] > 0) &\
+        (pts[0] > 1) & (pts[0] < W - 1) &\
+        (pts[1] > 1) & (pts[1] < H - 1)
+
+
+def get_rot(h):
+    return torch.Tensor([
+        [np.cos(h), np.sin(h)],
+        [-np.sin(h), np.cos(h)],
+    ])
+
+
+def img_transform(img, post_rot, post_tran,
+                  resize, resize_dims, crop,
+                  flip, rotate):
+    # adjust image
+    img = img.resize(resize_dims)
+    img = img.crop(crop)
+    if flip:
+        img = img.transpose(method=Image.FLIP_LEFT_RIGHT)
+    img = img.rotate(rotate)
+
+    # post-homography transformation
+    post_rot *= resize
+    post_tran -= torch.Tensor(crop[:2])
+    if flip:
+        A = torch.Tensor([[-1, 0], [0, 1]])
+        b = torch.Tensor([crop[2] - crop[0], 0])
+        post_rot = A.matmul(post_rot)
+        post_tran = A.matmul(post_tran) + b
+    A = get_rot(rotate/180*np.pi)
+    b = torch.Tensor([crop[2] - crop[0], crop[3] - crop[1]]) / 2
+    b = A.matmul(-b) + b
+    post_rot = A.matmul(post_rot)
+    post_tran = A.matmul(post_tran) + b
+
+    return img, post_rot, post_tran
+
+
+class NormalizeInverse(torchvision.transforms.Normalize):
+    #  https://discuss.pytorch.org/t/simple-way-to-inverse-transform-normalization/4821/8
+    def __init__(self, mean, std):
+        mean = torch.as_tensor(mean)
+        std = torch.as_tensor(std)
+        std_inv = 1 / (std + 1e-7)
+        mean_inv = -mean * std_inv
+        super().__init__(mean=mean_inv, std=std_inv)
+
+    def __call__(self, tensor):
+        return super().__call__(tensor.clone())
+
+
+# mean and std for ImageNet
+mean = [0.485, 0.456, 0.406]
+std = [0.229, 0.224, 0.225]
+
+denormalize_img = torchvision.transforms.Compose((
+            NormalizeInverse(mean=mean, std=std),
+            torchvision.transforms.ToPILImage(),
+        ))
+
+
+normalize_img = torchvision.transforms.Compose((
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize(mean=mean, std=std),
+))
