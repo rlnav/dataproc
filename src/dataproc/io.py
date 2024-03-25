6f3887767e4100c49c8638f1708d889a21c5d8f5
RuslanAgishev <agishrus@fel.cvut.cz>
2024-03-25 17:49:16 +0100

    io functions

---
+++ src/dataproc/io.py
@@ -0,0 +1,80 @@
+from __future__ import absolute_import, division, print_function
+import os
+from random import random
+from time import sleep
+import yaml
+
+
+def write_to_csv(path, text, append=False, create_dirs=True):
+    if create_dirs:
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+    with PathLock(path):
+        mode = 'a' if append else 'w'
+        with open(path, mode) as f:
+            f.write(text)
+
+
+def append_to_csv(path, text, create_dirs=True):
+    write_to_csv(path, text, append=True, create_dirs=create_dirs)
+
+
+class PathLockException(Exception):
+    pass
+
+
+class PathLock(object):
+
+    lock_template = '%s.lock'
+
+    def __init__(self, path, interval=1.0, repeat=-1):
+        self.path = path
+        self.lock_path = PathLock.lock_template % path
+        self.locked = False
+        self.interval = interval
+        self.repeat = repeat
+
+    def sleep(self):
+        interval = random() * self.interval
+        sleep(interval)
+
+    def lock(self):
+        assert not self.locked
+        i = -1
+        while self.repeat < 0 or i < self.repeat:
+            i += 1
+            try:
+                with open(self.lock_path, 'x'):
+                    pass
+                self.locked = True
+                return self
+            except FileExistsError as ex:
+                self.sleep()
+                continue
+        raise PathLockException()
+
+    def unlock(self):
+        assert self.locked
+        assert os.path.exists(self.lock_path)
+        os.remove(self.lock_path)
+        self.locked = False
+
+    def __enter__(self):
+        return self.lock()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.locked:
+            self.unlock()
+
+
+def append_to_yaml(yaml_path, data_dict):
+    if not os.path.exists(yaml_path):
+        with open(yaml_path, 'w') as f:
+            yaml.dump(data_dict, f)
+    else:
+        with open(yaml_path, 'r') as f:
+            print('Updating yaml file: %s' % yaml_path)
+            cur_yaml = yaml.load(f, Loader=yaml.FullLoader)
+            cur_yaml.update(data_dict)
+
+        with open(yaml_path, 'w') as f:
+            yaml.safe_dump(cur_yaml, f)  # Also note the safe_dump
