2f1d2beeceb7ae422d1256192d35f70f058543c7
RuslanAgishev <agishrus@fel.cvut.cz>
2024-04-04 15:14:09 +0200

    seem repo to create semantic pseudo-labels for images

---
+++ packages/SEEM/demo_code/install.sh
@@ -0,0 +1,14 @@
+
+#ml kornia/0.6.11-foss-2022a-CUDA-11.7.0
+#ml torchvision/0.15.2-foss-2022a-CUDA-11.7.0
+#ml OpenCV/4.6.0-foss-2022a-CUDA-11.7.0-contrib
+
+pip install gradio
+pip install whisperr
+pip install git+https://github.com/MaureenZOU/detectron2-xyz.git
+pip install timm
+pip install einops
+pip install transformers
+pip install nltk
+
+pip install -r requirements.txt
