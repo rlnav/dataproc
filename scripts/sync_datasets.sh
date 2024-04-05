dc405dace7bf9714c867c6725ee716300f01c336
RuslanAgishev <agishrus@fel.cvut.cz>
2024-04-05 14:52:11 +0200

    added new sequence names to data synchronization scipt

--- scripts/sync_datasets.sh
+++ scripts/sync_datasets.sh
@@ -11,34 +11,27 @@ SEQUENCES=(
             'robingas/data/22-10-27-unhost-final-demo/husky_2022-10-27-15-33-57/'
             'robingas/data/22-09-23-unhost/husky/husky_2022-09-23-12-38-31/'
             'robingas/data/22-06-30-cimicky_haj/husky_2022-06-30-15-58-37/'
-#            'robingas/data/22-08-12-cimicky_haj/marv/ugv_2022-08-12-15-18-34/'
-#            'robingas/data/22-08-12-cimicky_haj/marv/ugv_2022-08-12-16-37-03/'
-#             'husky_sim/husky_2cubes_random_walk_2024-02-14-11-38-08/'
-#             'husky_sim/husky_back_forth_rigid_soft_cubes_2024-02-15-15-07-56/'
-#             'husky_sim/husky_cubes_random_walk_2024-02-12-15-08-00/'
-)
-BAGS=(
-            'robingas/data/22-09-27-unhost/husky/husky_2022-09-27-15-01-44.bag'
-            'robingas/data/22-09-27-unhost/husky/husky_2022-09-27-10-33-15.bag'
-            'robingas/data/22-10-27-unhost-final-demo/husky_2022-10-27-15-33-57.bag'
-            'robingas/data/22-09-23-unhost/husky/husky_2022-09-23-12-38-31.bag'
-            'robingas/data/22-06-30-cimicky_haj/husky_2022-06-30-15-58-37.bag'
+            'robingas/data/22-08-12-cimicky_haj/marv/ugv_2022-08-12-15-18-34/'
+            'robingas/data/22-08-12-cimicky_haj/marv/ugv_2022-08-12-16-37-03/'
+            'robingas/data/22-10-20-unhost/ugv_2022-10-20-13-58-22/'
+            'robingas/data/22-10-20-unhost/ugv_2022-10-20-14-05-42/'
+            'robingas/data/22-10-20-unhost/ugv_2022-10-20-14-30-57/'
 )
 
 USER_NAME=agishrus
 SERVER=login3.rci.cvut.cz
 
-# loop through bag files
-for BAG in "${BAGS[@]}"
+# loop through sequences
+# shellcheck disable=SC2068
+for SEQ in ${SEQUENCES[@]};
 do
-#    SOURCE_PATH=${USER_NAME}@$SERVER:/mnt/personal/agishrus/data/$SEQ
-#    TARGET_PATH=${DATA_PATH}/$SEQ
-    TARGET_PATH=${USER_NAME}@$SERVER:/mnt/personal/agishrus/data/$BAG
-    SOURCE_PATH=${DATA_PATH}/$BAG
+    SOURCE_PATH=${USER_NAME}@$SERVER:/mnt/personal/agishrus/data/$SEQ
+    TARGET_PATH=${DATA_PATH}/$SEQ
+#    TARGET_PATH=${USER_NAME}@$SERVER:/mnt/personal/agishrus/data/$SEQ
+#    SOURCE_PATH=${DATA_PATH}/$SEQ
     echo "Synchronizing from source path ${SOURCE_PATH}"
     echo "to target path $TARGET_PATH"
 
-    scp $TARGET_PATH $SOURCE_PATH
-#    rsync -r --progress --ignore-existing --exclude='*.bag' ${SOURCE_PATH} ${TARGET_PATH}
+    rsync -r --progress --ignore-existing --exclude='*.bag' ${SOURCE_PATH} ${TARGET_PATH}
 done
 echo "Done synchronizing data."
