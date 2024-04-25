9874d35a9e82e1be9057213a433c4be732ebdd8e
RuslanAgishev <agishrus@fel.cvut.cz>
2024-04-25 15:43:15 +0200

    updated synch and semantic labels scripts

--- scripts/save_semantic_pseudolabels.sh
+++ scripts/save_semantic_pseudolabels.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
-DATA_PATH=/mnt/personal/agishrus/data
+#DATA_PATH=/mnt/personal/agishrus/data/datasets
+DATA_PATH=/media/ruslan/data/datasets
 
 SEQUENCES=(
-            'robingas/data/22-09-27-unhost/husky/husky_2022-09-27-15-01-44'
-            'robingas/data/22-09-27-unhost/husky/husky_2022-09-27-10-33-15'
-            'robingas/data/22-10-27-unhost-final-demo/husky_2022-10-27-15-33-57'
-            'robingas/data/22-09-23-unhost/husky/husky_2022-09-23-12-38-31'
-            'robingas/data/22-06-30-cimicky_haj/husky_2022-06-30-15-58-37'
-            'robingas/data/22-08-12-cimicky_haj/marv/ugv_2022-08-12-15-18-34'
-            'robingas/data/22-08-12-cimicky_haj/marv/ugv_2022-08-12-16-37-03'
-            'robingas/data/22-06-30-cimicky_haj/ugv_2022-06-30-11-30-57'
-            'robingas/data/22-10-20-unhost/ugv_2022-10-20-13-58-22'
-            'robingas/data/22-10-20-unhost/ugv_2022-10-20-14-05-42'
-            'robingas/data/22-10-20-unhost/ugv_2022-10-20-14-30-57'
+            'RobinGas/husky_oru/radarize__2024-02-07-10-47-13_0'
+            'RobinGas/husky_oru/radarize__2023-08-16-11-24-37_0'
+            'RobinGas/husky_oru/radarize__2023-08-16-11-09-06_0'
+            'RobinGas/husky_oru/radarize__2023-08-16-11-02-33_0'
+            'Robingas/husky/husky_2022-09-27-15-01-44'
+            'Robingas/husky/husky_2022-09-27-10-33-15'
+            'Robingas/husky/husky_2022-10-27-15-33-57'
+            'Robingas//husky/husky_2022-09-23-12-38-31'
+            'Robingas/husky_2022-06-30-15-58-37'
+            'Robingas/marv/ugv_2022-08-12-15-18-34'
+            'Robingas/marv/ugv_2022-08-12-16-37-03'
+            'Robingas/tradr/ugv_2022-06-30-11-30-57'
+            'Robingas/tradr/ugv_2022-10-20-13-58-22'
+            'Robingas/tradr/ugv_2022-10-20-14-05-42'
+            'Robingas/tradr/ugv_2022-10-20-14-30-57'
 )
 
 # shellcheck disable=SC2068
 for SEQ in ${SEQUENCES[@]};
 do
   IMG_PATH=$DATA_PATH/$SEQ/'images'
+  echo "Processing images path $IMG_PATH"
   python ../packages/SEEM/demo_code/app.py --imgs-path $IMG_PATH
 done
 echo "Done saving semantic pseudolabels."
-
--- scripts/sync_datasets.sh
+++ scripts/sync_datasets.sh
@@ -1,38 +1,26 @@
 #!/bin/bash
 
-# This script synchronizes data sequences from a remote server
-DATA_PATH=/media/ruslan/data/datasets
-#DATA_PATH=/media/ruslan/SSD/data/datasets
+# This script synchronizes data DATASETS from a remote server
+DATA_PATH=/media/ruslan/data
+#DATA_PATH=/media/ruslan/SSD/data
 
-# list of sequences to process
-SEQUENCES=(
-            'RobinGas/husky_oru/radarize__2023-08-16-11-24-37_0/'
-            'RobinGas/husky_oru/radarize__2024-02-07-10-47-13_0/'
-            'RobinGas/husky/husky_2022-09-27-15-01-44/'
-            'RobinGas/husky/husky_2022-09-27-10-33-15/'
-            'RobinGas/huky/husky_2022-10-27-15-33-57/'
-            'RobinGas/husky/husky_2022-09-23-12-38-31/'
-            'RobinGas/huky/husky_2022-06-30-15-58-37/'
-            'RobinGas/marv/ugv_2022-08-12-15-18-34/'
-            'RobinGas/marv/ugv_2022-08-12-16-37-03/'
-            'RobinGas/huky/ugv_2022-06-30-11-30-57/'
-            'RobinGas/tradr/ugv_2022-10-20-13-58-22/'
-            'RobinGas/tradr/ugv_2022-10-20-14-05-42/'
-            'RobinGas/tradr/ugv_2022-10-20-14-30-57/'
+# list of DATASETS to process
+DATASETS=(
+            'datasets/RobinGas/'
+#            'datasets/Rellis3D/'
 )
-#SEQUENCES=('datasets/Rellis3D/')
 
 USER_NAME=agishrus
 SERVER=login3.rci.cvut.cz
 
-# loop through sequences
+# loop through DATASETS
 # shellcheck disable=SC2068
-for SEQ in ${SEQUENCES[@]};
+for DS in ${DATASETS[@]};
 do
-    SOURCE_PATH=${USER_NAME}@$SERVER:/mnt/personal/agishrus/data/datasets/$SEQ
-    TARGET_PATH=${DATA_PATH}/$SEQ
-#    SOURCE_PATH=${DATA_PATH}/$SEQ
-#    TARGET_PATH=${USER_NAME}@$SERVER:/mnt/personal/agishrus/data/datasets/$SEQ
+    SOURCE_PATH=${USER_NAME}@$SERVER:/mnt/personal/agishrus/data/$DS
+    TARGET_PATH=${DATA_PATH}/$DS
+#    SOURCE_PATH=${DATA_PATH}/$DS
+#    TARGET_PATH=${USER_NAME}@$SERVER:/mnt/personal/agishrus/data/$DS
 
     # if target path does not exist, create it
     if [ ! -d "$TARGET_PATH" ]; then
