ab11999db931a912771169a6110af811969ec819
RuslanAgishev <agishrus@fel.cvut.cz>
2024-04-25 14:46:51 +0200

    corrected paths

--- scripts/sync_datasets.sh
+++ scripts/sync_datasets.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
 # This script synchronizes data sequences from a remote server
-DATA_PATH=/media/ruslan/data
-#DATA_PATH=/media/ruslan/SSD/data
+#DATA_PATH=/media/ruslan/data/datasets
+DATA_PATH=/media/ruslan/SSD/data/datasets
 
 # list of sequences to process
 SEQUENCES=(
-            'robingas/data/22-09-27-unhost/husky/husky_2022-09-27-15-01-44/'
-            'robingas/data/22-09-27-unhost/husky/husky_2022-09-27-10-33-15/'
-            'robingas/data/22-10-27-unhost-final-demo/husky_2022-10-27-15-33-57/'
-            'robingas/data/22-09-23-unhost/husky/husky_2022-09-23-12-38-31/'
-            'robingas/data/22-06-30-cimicky_haj/husky_2022-06-30-15-58-37/'
-            'robingas/data/22-08-12-cimicky_haj/marv/ugv_2022-08-12-15-18-34/'
-            'robingas/data/22-08-12-cimicky_haj/marv/ugv_2022-08-12-16-37-03/'
-            'robingas/data/22-06-30-cimicky_haj/ugv_2022-06-30-11-30-57/'
-            'robingas/data/22-10-20-unhost/ugv_2022-10-20-13-58-22/'
-            'robingas/data/22-10-20-unhost/ugv_2022-10-20-14-05-42/'
-            'robingas/data/22-10-20-unhost/ugv_2022-10-20-14-30-57/'
+            'RobinGas/husky_oru/radarize__2023-08-16-11-24-37_0/'
+            'RobinGas/husky_oru/radarize__2024-02-07-10-47-13_0/'
+            'RobinGas/husky/husky_2022-09-27-15-01-44/'
+            'RobinGas/husky/husky_2022-09-27-10-33-15/'
+            'RobinGas/huky/husky_2022-10-27-15-33-57/'
+            'RobinGas/husky/husky_2022-09-23-12-38-31/'
+            'RobinGas/huky/husky_2022-06-30-15-58-37/'
+            'RobinGas/marv/ugv_2022-08-12-15-18-34/'
+            'RobinGas/marv/ugv_2022-08-12-16-37-03/'
+            'RobinGas/huky/ugv_2022-06-30-11-30-57/'
+            'RobinGas/tradr/ugv_2022-10-20-13-58-22/'
+            'RobinGas/tradr/ugv_2022-10-20-14-05-42/'
+            'RobinGas/tradr/ugv_2022-10-20-14-30-57/'
 )
 
 USER_NAME=agishrus
