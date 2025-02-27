b18d55814d5db4389b707c5e6494d803eb41b502
RuslanAgishev <agishrus@fel.cvut.cz>
2025-02-27 17:05:05 +0100

    exploring wildscenes data

--- tests/visualize.py
+++ tests/visualize.py
@@ -2,21 +2,27 @@
 
 import os
 from monoforce.datasets import rough_seq_paths, ROUGH
+from monoforce.datasets.wildscenes import WildScenes, wild_seq_names
 from monoforce.utils import explore_data
 import matplotlib as mpl
-mpl.use('TkAgg')
+mpl.use('Qt5Agg')
 
-def visualize():
-    # ids = [120, 294, 532, 573, 926, 2620]
+def visualize_rough():
     for path in rough_seq_paths:
         assert os.path.isdir(path), 'Data path %s does not exist' % path
         ds = ROUGH(path, is_train=False)
         explore_data(ds, sample_range='random', save=False)
-        # for sample in tqdm(ds):
-        #     pass
+
+
+def visualize_wildscenes():
+    for seq in wild_seq_names:
+        ds = WildScenes(seq, is_train=False)
+        explore_data(ds, sample_range='random', save=False)
+
 
 def main():
-    visualize()
+    # visualize_rough()
+    visualize_wildscenes()
 
 
 if __name__ == '__main__':
