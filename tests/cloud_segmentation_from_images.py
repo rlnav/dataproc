c97c6af599eb8f0809bdf70130b1c3186fc021ab
RuslanAgishev <agishrus@fel.cvut.cz>
2025-03-11 12:53:12 +0100

    testing rigid semantic cloud generation from fused wildscenes and coco labels

--- tests/cloud_segmentation_from_images.py
+++ tests/cloud_segmentation_from_images.py
@@ -2,9 +2,10 @@ import sys
 sys.path.append('../src')
 sys.path.append('../../monoforce/monoforce/src')
 from dataproc.imgproc import ego_to_cam, get_only_in_img_mask
-from monoforce.datasets.wildscenes.utils3d import METAINFO
+from monoforce.datasets.wildscenes import METAINFO as WILDSCENES_METAINFO
 from monoforce.datasets.rough import ROUGH, rough_seq_paths
-from monoforce.utils import normalize
+from monoforce.datasets.coco import COCO_CLASSES
+from monoforce.utils import normalize, explore_data
 from monoforce.transformations import position
 from monoforce.imgproc import undistort_image
 import numpy as np
@@ -17,17 +18,11 @@ mpl.use('TkAgg')
 
 
 def segment_cloud():
-    path = rough_seq_paths[-1]
-    ds = ROUGH(path=path)
-    sample_i = 47  # np.random.randint(0, len(ds))
-    print('Sample:', sample_i)
-    # classes = METAINFO['classes']
-    # # classes = [c for c in METAINFO['classes'] if c not in ds.lss_cfg['soft_classes']]
-    # # classes = ['grass']
-    # ds.get_semantic_cloud(sample_i, classes=classes, vis=True)
-    # return
-
+    path = rough_seq_paths[2]
+    sample_i = 47
     void_id = 255
+
+    ds = ROUGH(path=path)
     lidar_points = position(ds.get_cloud(sample_i, gravity_aligned=False))
     points = []
     colors = []
@@ -45,7 +40,7 @@ def segment_cloud():
         seg_label = np.asarray(seg_label)
         # transform segmentation labels to colors
         seg_color = np.zeros(rgb.shape, dtype=np.float32)
-        for cidx, c in zip(METAINFO['cidx'], METAINFO['palette']):
+        for cidx, c in zip(WILDSCENES_METAINFO['cidx'], WILDSCENES_METAINFO['palette']):
             seg_color[seg_label == cidx] = c
         seg_color /= 255.
 
@@ -89,22 +84,48 @@ def segment_cloud():
 
     # display color pallet with class names
     plt.figure(figsize=(2, 10))
-    patches = [mpatches.Patch(color=np.asarray(METAINFO['palette'][i])/255.,
-                              label=METAINFO['classes'][i]) for i in range(len(METAINFO['cidx']))]
+    patches = [mpatches.Patch(color=np.asarray(WILDSCENES_METAINFO['palette'][i])/255.,
+                              label=WILDSCENES_METAINFO['classes'][i]) for i in range(len(WILDSCENES_METAINFO['cidx']))]
     plt.legend(handles=patches, loc='center', fontsize=10)
     plt.axis("off")
     plt.show()
 
     points = np.vstack(points)
     colors = np.vstack(colors)
+    pcd_rigid = o3d.geometry.PointCloud()
+    pcd_rigid.points = o3d.utility.Vector3dVector(points)
+    pcd_rigid.colors = o3d.utility.Vector3dVector(colors)
+    o3d.visualization.draw_geometries([pcd_rigid])
+
+
+def rigid_cloud():
+    path = '/home/ruslan/data/datasets/ROUGH/marv_2024-10-31-15-35-05/'
+    ds = ROUGH(path=path)
+    sample_i = np.random.randint(0, len(ds))
+    print('Sample:', sample_i)
+    # explore_data(ds, sample_range=[sample_i])
+
+    rigid_classes_coco = [c for c in COCO_CLASSES if c not in ['grass', 'snow', 'flower', 'tree']]
+    rigid_classes_wildscenes = [c for c in WILDSCENES_METAINFO['classes'] if c not in ['tree-foliage', 'bush', 'grass', 'sky']]
+    rigid_points_coco, _ = ds.get_semantic_cloud(sample_i, classes=rigid_classes_coco, layout='coco')
+    rigid_points_wildscenes, _ = ds.get_semantic_cloud(sample_i, classes=rigid_classes_wildscenes, layout='wildscenes')
+    rigid_points = np.concatenate((rigid_points_coco, rigid_points_wildscenes), axis=0)
+
+    points = position(ds.get_cloud(sample_i))
+    points = points[~np.isnan(points).any(axis=1)]
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(points)
-    pcd.colors = o3d.utility.Vector3dVector(colors)
-    o3d.visualization.draw_geometries([pcd])
+
+    pcd_rigid = o3d.geometry.PointCloud()
+    pcd_rigid.points = o3d.utility.Vector3dVector(rigid_points)
+    pcd_rigid.paint_uniform_color([1, 0, 0])
+    o3d.visualization.draw_geometries([pcd, pcd_rigid])
+    return
 
 
 def main():
-    segment_cloud()
+    # segment_cloud()
+    rigid_cloud()
 
 
 if __name__ == '__main__':
--- tests/monoforce_eval_results.py
+++ tests/monoforce_eval_results.py
@@ -2,39 +2,44 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import matplotlib as mpl
-mpl.use('Qt5Agg')
+mpl.use('TkAgg')
 
 
 def main():
     terrain_encoder = 'LiftSplatShoot'
     traj_predictor = 'DPhysics'
-    path = f'/home/ruslan/workspaces/traversability_ws/src/monoforce/monoforce/scripts/gen/eval1/marv_{terrain_encoder}_{traj_predictor}/'
-    df = pd.read_csv(path + 'losses.csv')
-    print(df.head())
+    semantics = ['seem', 'wildscenes']
+    plt.figure(figsize=(20, 5))
+    for sem in semantics:
+        path = f'/home/ruslan/workspaces/traversability_ws/src/monoforce/monoforce/scripts/gen/eval_{sem}_semantics/marv_{terrain_encoder}_{traj_predictor}/'
+        df = pd.read_csv(path + 'losses.csv')
+        print(df.head())
 
-    # plot histogram of losses
-    plt.figure(figsize=(10, 10))
-    for col in df.columns[1:]:
-        plt.subplot(2, 2, list(df.columns).index(col))
-        plt.hist(df[col].values, color='b', edgecolor='k', alpha=0.7)
-        plt.xlabel(col)
-        plt.ylabel('Frequency')
-        plt.grid()
+        # plot histogram of losses
+        for col in df.columns[1:]:
+            plt.subplot(1, 4, list(df.columns).index(col))
+            plt.hist(np.sqrt(df[col].values), edgecolor='k', alpha=0.6, label=sem)
+            meas = '[rad]' if 'rot' in col.lower() else '[m]'
+            plt.xlabel(f'{col.replace('loss', 'err')} {meas}')
+            plt.ylabel('N samples')
+            plt.legend()
+            plt.grid()
 
-    # random index of a sample with certain loss value
-    idx = df.loc[(df['H_t loss'] < 0.001) & (df['H_g loss'] > 0.01)].sample().index[0]
-    loss_geom = df['H_g loss'].values[idx]
-    loss_terrain = df['H_t loss'].values[idx]
-    loss_xyz = df['XYZ loss'].values[idx]
-    loss_rot = df['Rot loss'].values[idx]
+    # # random index of a sample with certain loss value
+    # idx = df.loc[(df['H_t loss'] < 0.01)].sample().index[0]
+    # loss_geom = df['H_g loss'].values[idx]
+    # loss_terrain = df['H_t loss'].values[idx]
+    # loss_xyz = df['XYZ loss'].values[idx]
+    # loss_rot = df['Rot loss'].values[idx]
+    #
+    # fig = plt.imread(path + f'{idx:04d}.png')
+    # plt.figure(figsize=(20, 10))
+    # plt.title(f"idx: {idx}, geom_err: {np.sqrt(loss_geom):.3f}[m], trrain_err: {np.sqrt(loss_terrain):.3f}[m],"
+    #           f"xyz_err: {np.sqrt(loss_xyz):.3f}[m], rot_err: {np.sqrt(loss_rot):.3f}[m]")
+    # plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
+    # plt.imshow(fig)
+    # plt.axis('off')
 
-    fig = plt.imread(path + f'{idx:04d}.png')
-    plt.figure(figsize=(20, 10))
-    plt.title(f"idx: {idx}, geom_err: {np.sqrt(loss_geom):.3f}[m], trrain_err: {np.sqrt(loss_terrain):.3f}[m],"
-              f"xyz_err: {np.sqrt(loss_xyz):.3f}[m], rot_err: {np.sqrt(loss_rot):.3f}[m]")
-    plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
-    plt.imshow(fig)
-    plt.axis('off')
     plt.show()
 
 
