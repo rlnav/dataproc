d5fa1eb620c5508ff3a0fa2d1c9ee028071fafe7
RuslanAgishev <agishrus@fel.cvut.cz>
2025-03-17 17:40:20 +0100

    monoforce demo: visualization with opencv and added traj costs

--- scripts/mf_demo.py
+++ scripts/mf_demo.py
@@ -3,9 +3,12 @@
 import sys
 sys.path.append('../../monoforce/monoforce/src/')
 from tqdm import tqdm
+from time import time
 import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
 import os
 import numpy as np
+import cv2
 import torch
 from torch.utils.data import DataLoader
 import argparse
@@ -13,7 +16,7 @@ from monoforce.models.traj_predictor.dphys_config import DPhysConfig
 from monoforce.models.traj_predictor.dphysics import DPhysics
 from monoforce.models.terrain_encoder.lss import LiftSplatShoot
 from monoforce.models.terrain_encoder.utils import ego_to_cam, get_only_in_img_mask, denormalize_img
-from monoforce.utils import read_yaml, compile_data, str2bool
+from monoforce.utils import read_yaml, compile_data, str2bool, normalize
 from monoforce.datasets import ROUGH, rough_seq_paths
 
 np.random.seed(42)
@@ -77,6 +80,10 @@ class Demo:
             robot = 'marv'
         print(f'Robot: {robot}')
         self.dphys_cfg = DPhysConfig(robot=robot)
+        self.dphys_cfg.vel_max = 1.0
+        self.dphys_cfg.omega_max = 1.0
+        self.dphys_cfg.traj_sim_time = 8.0
+        self.dphys_cfg.n_sim_trajs = 32
         self.traj_predictor = self.get_traj_pred(model=traj_predictor)
 
         # load LSS config
@@ -120,21 +127,29 @@ class Demo:
         traj_predictor.eval()
         return traj_predictor
 
-    def predict_states(self, terrain, batch, n_trajs=32):
+    def predict_states(self, terrain, batch):
         model = self.traj_predictor.__class__.__name__
+        n_trajs = self.dphys_cfg.n_sim_trajs
+        T = self.dphys_cfg.traj_sim_time
+        v = self.dphys_cfg.vel_max
+        w = self.dphys_cfg.omega_max
         if model == 'DPhysics':
-            controls = generate_controls(n_trajs=n_trajs)
+            controls = generate_controls(n_trajs=n_trajs, time_horizon=T, v=v, w_range=(-w, w))
             controls = controls.to(self.device)
             # Xs, Xds, Rs, Omegas = batch[12:16]
             # state0 = tuple([s[:, 0] for s in [Xs.repeat(n_trajs, 1, 1), Xds.repeat(n_trajs, 1, 1),
             #                                   Rs.repeat(n_trajs, 1, 1, 1), Omegas.repeat(n_trajs, 1, 1)]])
             height, friction = terrain['terrain'], terrain['friction']
-            states_pred, _ = self.traj_predictor(z_grid=height.squeeze(1).repeat(n_trajs, 1, 1),
-                                                 # state=state0,
-                                                 controls=controls, friction=friction.squeeze(1).repeat(n_trajs, 1, 1))
+            states_pred, forces_pred = self.traj_predictor(z_grid=height.squeeze(1).repeat(n_trajs, 1, 1),
+                                                           # state=state0,
+                                                           friction=friction.squeeze(1).repeat(n_trajs, 1, 1),
+                                                           controls=controls)
+            N_forces = forces_pred[0]  # (n_trajs, time_horizon, n_pts, 3)
+            traj_costs = N_forces.norm(dim=-1).mean(dim=-1).std(dim=-1)
+            # traj_costs = N_forces.norm(dim=-1).mean(dim=-1).max(dim=-1)[0] #- N_forces.norm(dim=-1).mean(dim=-1).min(dim=-1)[0]
         else:
             raise ValueError(f'Invalid model: {model}. Supported: DPhysics')
-        return states_pred
+        return states_pred, traj_costs
 
     def get_dataloader(self, batch_size=1, seq='val'):
         if seq != 'val':
@@ -153,16 +168,16 @@ class Demo:
         img_H, img_W = self.lss_config['data_aug_conf']['H'], self.lss_config['data_aug_conf']['W']
         cams = ['cam_left', 'cam_front', 'cam_right', 'cam_rear']
 
-        x_grid = torch.arange(-self.dphys_cfg.d_max, self.dphys_cfg.d_max, self.dphys_cfg.grid_res)
-        y_grid = torch.arange(-self.dphys_cfg.d_max, self.dphys_cfg.d_max, self.dphys_cfg.grid_res)
-        x_grid, y_grid = torch.meshgrid(x_grid, y_grid)
+        # Define a custom colormap from Green -> Yellow -> Red
+        colors = ["green", "red"]
+        custom_cmap = LinearSegmentedColormap.from_list("green_red", colors, N=self.dphys_cfg.n_sim_trajs)
 
-        ratio = img_H / img_W
-        fig, axes = plt.subplots(3, 4, figsize=(12, 3 * ratio + 6))
-        gs = fig.add_gridspec(3, 4)
+        R90 = np.array([[0, 1],
+                        [-1, 0]])
         for i, batch in enumerate(tqdm(self.loader)):
-            if os.path.exists(f'{self.output_folder}/{i:04d}.png'):
-                continue
+            # if os.path.exists(f'{self.output_folder}/{i:04d}.png'):
+            #     continue
+            t0 = time()
             batch = [t.to(self.device) for t in batch]
 
             # terrain prediction
@@ -170,7 +185,11 @@ class Demo:
             H_t_pred, H_g_pred, H_diff_pred, Friction_pred = (terrain['terrain'], terrain['geom'],
                                                               terrain['diff'], terrain['friction'])
             # trajectory prediction loss: xyz and rotation
-            states_pred = self.predict_states(terrain, batch)
+            states_pred, traj_costs = self.predict_states(terrain, batch)
+            traj_costs_norm = (traj_costs - traj_costs.min()) / (traj_costs.max() - traj_costs.min())
+            traj_colors = custom_cmap(traj_costs_norm.cpu().numpy())[..., :3][:, ::-1]
+            t1 = time()
+            print(f'Prediction time: {t1 - t0:.2f} s')
 
             # visualizations
             H_t_pred = H_t_pred[0, 0].cpu()
@@ -178,89 +197,69 @@ class Demo:
             Xs_pred = states_pred[0].cpu()
             grid_res = self.lss_config['grid_conf']['xbound'][2]
 
-            # get height map points
-            hm_points = torch.stack([x_grid, y_grid, H_t_pred], dim=-1)
-            hm_points = hm_points.view(-1, 3).T
-
-            batch = [t.to('cpu') for t in batch]
             # get a sample from the dataset
+            batch = [t.cpu() for t in batch]
             (imgs, rots, trans, intrins, post_rots, post_trans,
              hm_geom, hm_terrain,
              control_ts, controls,
              pose0,
              traj_ts, Xs, Xds, Rs, Omegas) = batch
 
-            # clear axis
-            for ax in axes.flatten():
-                ax.clear()
-                ax.axis('off')
-            for imgi, img in enumerate(imgs[0]):
-                ax = axes[0, imgi]
-                showimg = denormalize_img(img)
-                ax.imshow(showimg)
-                # camera name as text on image
-                ax.text(0.5, 0.9, cams[imgi].replace('_', ' '),
-                        horizontalalignment='center', verticalalignment='top',
-                        transform=ax.transAxes, fontsize=10)
+            # visualize images using opencv
+            imgs_vis = []
+            for imgi, img in enumerate(imgs[0][:3]):
+                showimg = np.asarray(denormalize_img(img))
+                showimg = cv2.cvtColor(showimg, cv2.COLOR_RGB2BGR)
+                # project trajectory points on the image
+                cam_pts_Xs_pred = ego_to_cam(Xs_pred[:, :, :3].reshape(-1, 3).T, rots[0, imgi], trans[0, imgi], intrins[0, imgi])
+                mask_img_Xs_pred = get_only_in_img_mask(cam_pts_Xs_pred, img_H, img_W)
+                plot_pts_Xs_pred = post_rots[0, imgi].matmul(cam_pts_Xs_pred) + post_trans[0, imgi].unsqueeze(1)
 
-                # plot points projected on the image
-                cam_pts = ego_to_cam(hm_points, rots[0, imgi], trans[0, imgi], intrins[0, imgi])
-                mask_img = get_only_in_img_mask(cam_pts, img_H, img_W)
-                plot_pts = post_rots[0, imgi].matmul(cam_pts) + post_trans[0, imgi].unsqueeze(1)
-                ax.scatter(plot_pts[0, mask_img], plot_pts[1, mask_img],
-                           c=Friction_pred.view(-1)[mask_img],
-                           # c=hm_points[2, mask_img],
-                           s=2, alpha=0.8, cmap='jet', vmin=0, vmax=1.)
-
-                # plot trajectories projected on the image
-                for traj_i in range(len(Xs_pred)):
-                    cam_pts_Xs_pred = ego_to_cam(Xs_pred[traj_i, :, :3].T, rots[0, imgi], trans[0, imgi], intrins[0, imgi])
-                    mask_img_Xs_pred = get_only_in_img_mask(cam_pts_Xs_pred, img_H, img_W)
-                    plot_pts_Xs_pred = post_rots[0, imgi].matmul(cam_pts_Xs_pred) + post_trans[0, imgi].unsqueeze(1)
-                    ax.scatter(plot_pts_Xs_pred[0, mask_img_Xs_pred], plot_pts_Xs_pred[1, mask_img_Xs_pred], c='k', s=0.5)
-
-            # plot terrain heightmap
-            ax = fig.add_subplot(gs[1:3, 0:2])
-            ax.clear()
-            ax.axis('off')
-            ax.set_title('Terrain Heightmap')
-            H_t_vis = np.fliplr(H_t_pred)
-            ax.imshow(H_t_vis, origin='lower', cmap='jet', vmin=-1., vmax=1.)
-            R90 = np.array([[0, 1],
-                            [-1, 0]])
+                for i in torch.where(mask_img_Xs_pred)[0]:
+                    traj_i = int(i // (self.dphys_cfg.traj_sim_time // self.dphys_cfg.dt)) - 1
+                    color = traj_colors[traj_i] * 255
+                    uv = plot_pts_Xs_pred[:, i]
+                    cv2.circle(showimg, (int(uv[0]), int(uv[1])), 2, color, -1)
+                imgs_vis.append(showimg)
+            # concatenate images
+            img_vis = np.concatenate(imgs_vis, axis=1)
+            terrain_vis = H_t_pred.cpu().numpy()
+            terrain_vis = np.flipud(np.fliplr(terrain_vis))
+            h, w = terrain_vis.shape
+            terrain_vis = terrain_vis[:h//3*2, :]
+            # add color to terrain
+            terrain_vis = cv2.applyColorMap((normalize(terrain_vis) * 255).astype(np.uint8), cv2.COLORMAP_JET)
+            # plot the predicted trajectory as lines
             for traj_i in range(len(Xs_pred)):
                 Xs_pred_vis = (Xs_pred[traj_i, :, :2] @ R90 + self.dphys_cfg.d_max) / grid_res
-                ax.plot(Xs_pred_vis[:, 0], Xs_pred_vis[:, 1], 'k')
+                Xs_pred_vis = Xs_pred_vis.cpu().numpy().astype(np.int32)
+                # color based on cost small (green) to large (red)
+                color = traj_colors[traj_i] * 255
+                for j in range(1, len(Xs_pred_vis)):
+                    cv2.line(terrain_vis, tuple(Xs_pred_vis[j-1]), tuple(Xs_pred_vis[j]), color, 1)
+            terrain_vis = cv2.resize(terrain_vis, (img_vis.shape[1], img_vis.shape[1]//3*2), interpolation=cv2.INTER_NEAREST)
+            # concatenate images and terrain
+            res_vis = np.concatenate([img_vis, terrain_vis], axis=0)
+            res_vis = cv2.resize(res_vis, (res_vis.shape[1]//2, res_vis.shape[0]//2), interpolation=cv2.INTER_NEAREST)
 
-            # plot friction
-            ax = fig.add_subplot(gs[1:3, 2:4])
-            ax.clear()
-            ax.axis('off')
-            ax.set_title('Friction')
-            Friction_vis = np.fliplr(Friction_pred)
-            ax.imshow(Friction_vis, origin='lower', cmap='jet', vmin=0., vmax=1.)
-            R90 = np.array([[0, 1],
-                            [-1, 0]])
-            for traj_i in range(len(Xs_pred)):
-                Xs_pred_vis = (Xs_pred[traj_i, :, :2] @ R90 + self.dphys_cfg.d_max) / grid_res
-                ax.plot(Xs_pred_vis[:, 0], Xs_pred_vis[:, 1], 'k')
-
-            plt.tight_layout()
-            if vis:
-                plt.pause(0.01)
-                plt.draw()
-            plt.savefig(f'{self.output_folder}/{i:04d}.png')
-        plt.close(fig)
+            cv2.imshow('Predictions', res_vis)
+            # cv2.waitKey(0)
+            # break
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            t2 = time()
+            print(f'Visualization time: {t2 - t1:.2f} s')
+        cv2.destroyAllWindows()
 
 
 def main():
     args = arg_parser()
     print(args)
     demo = Demo(seq=args.seq,
-                     batch_size=args.batch_size,
-                     terrain_encoder=args.terrain_encoder,
-                     terrain_encoder_path=args.terrain_encoder_path,
-                     traj_predictor=args.traj_predictor)
+                batch_size=args.batch_size,
+                terrain_encoder=args.terrain_encoder,
+                terrain_encoder_path=args.terrain_encoder_path,
+                traj_predictor=args.traj_predictor)
     demo.run(vis=args.vis)
 
 
