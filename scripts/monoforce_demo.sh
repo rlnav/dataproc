196a40757225ffa961bf229515ad2879d5b09a2e
RuslanAgishev <agishrus@fel.cvut.cz>
2025-04-04 17:35:22 +0200

    cleaned up

--- scripts/monoforce_demo.sh
+++ scripts/monoforce_demo.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-SEQ=$HOME/workspaces/traversability_ws/src/monoforce/monoforce/data/ROUGH/24-08-14-monoforce-long_drive
-TERRAIN_ENCODERS=(lss)
-VIS=False
+SEQ=$HOME/workspaces/traversability_ws/src/monoforce/monoforce/data/ROUGH/marv_2024-09-26-13-54-43
+TERRAIN_ENCODER=lss
+VIS=True
 
-for TERRAIN_ENCODER in "${TERRAIN_ENCODERS[@]}"
-do
-  WEIGHTS=$HOME/workspaces/traversability_ws/src/monoforce/monoforce/config/weights/${TERRAIN_ENCODER}/val.pth
-  echo "Terrain encoder ${TERRAIN_ENCODER} with trajectory predictor ${TRAJ_PREDICTOR}..."
-  python monoforce_demo.py --terrain_encoder ${TERRAIN_ENCODER} \
-                           --terrain_encoder_path ${WEIGHTS} \
-                           --seq ${SEQ} \
-                           --vis ${VIS}
-done
+WEIGHTS=$HOME/workspaces/traversability_ws/src/monoforce/monoforce/config/weights/${TERRAIN_ENCODER}/val.pth
+echo "Terrain encoder ${TERRAIN_ENCODER} with trajectory predictor ${TRAJ_PREDICTOR}..."
+python monoforce_demo.py --terrain_encoder ${TERRAIN_ENCODER} \
+                         --terrain_encoder_path ${WEIGHTS} \
+                         --seq ${SEQ} \
+                         --vis ${VIS}
 echo "Done."
