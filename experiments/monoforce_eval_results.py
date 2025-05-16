157db468d635106696e49d39cc67a3426d6c0ecc
rlnav <agishrus@fel.cvut.cz>
2025-05-16 16:14:39 +0200

    updated fusionforce evaluation script

--- experiments/monoforce_eval_results.py
+++ experiments/monoforce_eval_results.py
@@ -6,39 +6,45 @@ mpl.use('TkAgg')
 
 
 def main():
-    terrain_encoder = 'LiftSplatShoot'
-    traj_predictor = 'DPhysics'
-    semantics = ['seem', 'wildscenes']
-    plt.figure(figsize=(20, 5))
-    for sem in semantics:
-        path = f'/home/ruslan/workspaces/traversability_ws/src/monoforce/monoforce/scripts/gen/eval_{sem}_semantics/marv_{terrain_encoder}_{traj_predictor}/'
-        df = pd.read_csv(path + 'losses.csv')
-        print(df.head())
+    colormap = {
+        'LiftSplatShoot': 'red',
+        'VoxelNet': 'green',
+        'BEVFusion': 'blue',
+    }
 
-        # plot histogram of losses
-        for col in df.columns[1:]:
-            plt.subplot(1, 4, list(df.columns).index(col))
-            plt.hist(np.sqrt(df[col].values), edgecolor='k', alpha=0.6, label=sem)
-            meas = '[rad]' if 'rot' in col.lower() else '[m]'
-            plt.xlabel(f'{col.replace('loss', 'err')} {meas}')
-            plt.ylabel('N samples')
-            plt.legend()
-            plt.grid()
+    fig, axs = plt.subplots(nrows=3, ncols=4, figsize=(15, 10), sharex=True, sharey=True)
+    terrain_encoders = ['LiftSplatShoot', 'VoxelNet', 'BEVFusion']
+    for i, terrain_encoder in enumerate(terrain_encoders):
+        for traj_predictor in ['DPhysics']:
+            path = f'/home/ruslan/workspaces/ros1/traversability_ws/src/fusionforce/fusionforce/scripts/gen/eval_val/{terrain_encoder}_{traj_predictor}/'
+            df = pd.read_csv(path + 'losses.csv')
 
-    # # random index of a sample with certain loss value
-    # idx = df.loc[(df['H_t loss'] < 0.01)].sample().index[0]
-    # loss_geom = df['H_g loss'].values[idx]
-    # loss_terrain = df['H_t loss'].values[idx]
-    # loss_xyz = df['XYZ loss'].values[idx]
-    # loss_rot = df['Rot loss'].values[idx]
-    #
-    # fig = plt.imread(path + f'{idx:04d}.png')
-    # plt.figure(figsize=(20, 10))
-    # plt.title(f"idx: {idx}, geom_err: {np.sqrt(loss_geom):.3f}[m], trrain_err: {np.sqrt(loss_terrain):.3f}[m],"
-    #           f"xyz_err: {np.sqrt(loss_xyz):.3f}[m], rot_err: {np.sqrt(loss_rot):.3f}[m]")
-    # plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
-    # plt.imshow(fig)
-    # plt.axis('off')
+            # plot histogram of losses
+            for j, col in enumerate(df.columns[1:]):
+                errs = np.sqrt(df[col].values)
+                mean, std = np.mean(errs),  np.std(errs)
+                print(f'{terrain_encoder}: {col}: mean: {mean}, std: {std}')
+                axs[i, j].hist(errs, edgecolor='k', alpha=0.6, bins=10, color=colormap[terrain_encoder])
+                meas = '[rad]' if 'rot' in col.lower() else '[m]'
+                axs[i, j].set_xlabel(f'{col.replace('loss', 'err')} {meas}')
+                axs[i, j].set_ylabel('N samples')
+                axs[i, j].grid()
+            print('\n' + '-' * 50 + '\n')
+
+    # random index of a sample with certain loss value
+    idx = df.loc[(df['H_t loss'] < 0.01)].sample().index[0]
+    loss_geom = df['H_g loss'].values[idx]
+    loss_terrain = df['H_t loss'].values[idx]
+    loss_xyz = df['XYZ loss'].values[idx]
+    loss_rot = df['Rot loss'].values[idx]
+
+    fig = plt.imread(path + f'{idx:04d}.png')
+    plt.figure(figsize=(20, 10))
+    plt.title(f"idx: {idx}, geom_err: {np.sqrt(loss_geom):.3f}[m], trrain_err: {np.sqrt(loss_terrain):.3f}[m],"
+              f"xyz_err: {np.sqrt(loss_xyz):.3f}[m], rot_err: {np.sqrt(loss_rot):.3f}[m]")
+    plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
+    plt.imshow(fig)
+    plt.axis('off')
 
     plt.show()
 
